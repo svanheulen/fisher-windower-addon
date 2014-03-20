7f199c3a188b1741a9eee9fbc787d859e4377018
Seth VanHeulen <svanheulen@structart.com>
2014-03-19 22:57:24 -0400

    added bait equip, fixed catch and release packets

--- fisher.lua
+++ fisher.lua
@@ -16,28 +16,66 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 _addon.name = 'fisher'
-_addon.version = '1.0.0'
+_addon.version = '1.0.1'
 _addon.command = 'fisher'
 _addon.author = 'Seth VanHeulen'
 
-catch_id = '\13\0\228\2'
+bait_id = 17400 -- sinking minnow
+fish_id = '\13\0\228\2' -- hakuryu
 catch_key = nil
 catch_delay = 20
 catch_time = nil
-cast_delay = 3
+cast_delay = 4
 cast_time = nil
 running = false
 
+-- binary helper functions
+
+function pack_uint16(num)
+    return string.char(num % 256, math.floor(num / 256))
+end
+
+function pack_uint32(num)
+    str = string.char(num % 256)
+    num = math.floor(num / 256)
+    str = str .. string.char(num % 256)
+    num = math.floor(num / 256)
+    str = str .. string.char(num % 256)
+    num = math.floor(num / 256)
+    return str .. string.char(num % 256)
+end
+
+-- bait helper functions
+
+function check_bait()
+    items = windower.ffxi.get_items()
+    if items.equipment.ammo == 0 or items.inventory[items.equipment.ammo].id ~= bait_id then
+        return false
+    end
+    return true
+end
+
+function equip_bait()
+    for slot,item in pairs(windower.ffxi.get_items().inventory) do
+        if item.id == bait_id and item.status == 0 then
+            windower.ffxi.set_equip(slot, 3)
+            return true
+        end
+    end
+    return false
+end
+
+-- event callback functions
+
 function check_incoming_chunk(id, original, modified, injected, blocked)
-    if running == true then
+    if running then
         if id == 0x115 then
-            if catch_id == original:sub(11, 14) then
+            if fish_id == original:sub(11, 14) then
                 catch_key = original:sub(21)
                 catch_time = os.time() + catch_delay
             else
                 player = windower.ffxi.get_player()
-                payload = '\16\11\0\0' .. string.char(player.id % 256, player.id / 256) .. '\0\0\200\0\0\0' .. string.char(player.index % 256, player.index / 256) .. '\3\0\0\0\0\0'
-                windower.packets.inject_outgoing(0x110, payload)
+                windower.packets.inject_outgoing(0x110, '\16\11\0\0' .. pack_uint32(player.id) .. '\200\0\0\0' .. pack_uint16(player.index) .. '\3\0\0\0\0\0')
             end
         elseif id == 0x53 then
             cast_delay = cast_delay + 1
@@ -47,20 +85,24 @@ function check_incoming_chunk(id, original, modified, injected, blocked)
 end
 
 function check_outgoing_chunk(id, original, modified, injected, blocked)
-    if running == true and id == 0x110 and original:byte(15) == 4 then
-        cast_time = os.time() + cast_delay
+    if running and id == 0x110 and original:byte(15) == 4 then
+        if check_bait() or equip_bait() then
+            cast_time = os.time() + cast_delay
+            running = true
+        end
     end
 end
 
 function check_prerender()
-    if catch_time ~= nil and os.time() >= catch_time then
-        catch_time = nil
-        player = windower.ffxi.get_player()
-        payload = '\16\11\0\0' .. string.char(player.id % 256, player.id / 256) .. '\0\0\0\0\0\0' .. string.char(player.index % 256, player.index / 256) .. '\3\0' .. catch_key
-        windower.packets.inject_outgoing(0x110, payload)
-    elseif cast_time ~= nil and os.time() >= cast_time then
-        cast_time = nil
-        windower.send_command('input /fish')
+    if running then
+        if catch_time ~= nil and os.time() >= catch_time then
+            catch_time = nil
+            player = windower.ffxi.get_player()
+            windower.packets.inject_outgoing(0x110, '\16\11\0\0' .. pack_uint32(player.id) .. '\0\0\0\0' .. pack_uint16(player.index) .. '\3\0' .. catch_key)
+        elseif cast_time ~= nil and os.time() >= cast_time then
+            cast_time = nil
+            windower.send_command('input /fish')
+        end
     end
 end
 
@@ -70,10 +112,14 @@ function fisher_command(...)
         cast_time = nil
         running = false
     elseif #arg == 1 and arg[1]:lower() == 'start' then
-        cast_time = os.time()
-        running = true
+        if check_bait() or equip_bait() then
+            cast_time = os.time() + cast_delay
+            running = true
+        end
     end
 end
+
+-- register event callbacks
 
 windower.register_event('incoming chunk', check_incoming_chunk)
 windower.register_event('outgoing chunk', check_outgoing_chunk)
