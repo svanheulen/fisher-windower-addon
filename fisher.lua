399b7c8a1475628fc8884430739fc9d4eb4654e9
svanheulen <svanheulen@gmail.com>
2014-04-11 17:39:06 -0400

    added temporary fix for status change after login

--- fisher.lua
+++ fisher.lua
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- addon information
 
 _addon.name = 'fisher'
-_addon.version = '2.0.0'
+_addon.version = '2.0.1'
 _addon.command = 'fisher'
 _addon.author = 'Seth VanHeulen'
 
@@ -325,7 +325,7 @@ end
 -- event callback functions
 
 function check_status_change(new_status_id, old_status_id)
-    if running then
+    if running and new_status_id ~= 0 then
         message(0, 'status changed')
         message(3, 'status new: %d, old: %d':format(new_status_id, old_status_id))
         fisher_command('stop')
